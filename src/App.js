76db242e43e328c74e87c5a12ac957d6a7d648a1
navyaanzaheen <navyaanzaheen@gmail.com>
2025-01-19 13:04:11 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+
+let name = "Navyaan"
+
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+   </nav>
+
+   <div className="container">
+    <h1>hello {name} </h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius accusamus doloribus totam suscipit officiis velit eum quia libero excepturi consequuntur, tempora sint. Sit rerum ullam id dignissimos est magni vitae optio quam dicta. Sit.</p>
+    
+    </div>    
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/dist/App.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+require("./App.css");
+
+// import logo from './logo.svg';
+function App() {
+  return div.blank;
+}
+
+var _default = App;
+exports["default"] = _default;
